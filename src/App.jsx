6b009e236506163e8f22ba7520143bb36961a7a9
Mauricio Chiapetta <m.faisca1234@gmail.com>
2024-09-05 16:38:20 -0300

    adicionando icones e mais tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import { useState } from "react";
 import Navbar from "./components/Navbar";
 import { useRef } from "react";
+import {
+  faPlusCircle,
+  faTrash,
+  faTrashCan,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function App() {
   const [todos, setTodos] = useState([]);
@@ -48,10 +54,10 @@ function App() {
               value={task}
               onChange={(e) => setTask(e.target.value)}
               placeholder="Adicione uma nova tarefa"
-              className="bg-zinc-800 border-none text-white placeholder-zinc-500 font-medium outline-none focus:border-purple-800 focus:ring-1 focus:ring-violet-400 rounded-lg px-2 py-3 mr-2 w-5/12"
+              className="bg-zinc-800 border-none text-white placeholder-zinc-500 font-medium outline-none focus:border-purple-800 focus:ring-1 focus:ring-violet-400 rounded-lg px-2 py-3 mr-2 w-5/12 shadow-lg"
             />
-            <button className="transition duration-300 bg-sky-600 hover:bg-sky-500 text-white p-5 py-3 rounded-lg font-semibold">
-              Criar<span>+</span>
+            <button className="transition duration-300 bg-sky-600 hover:bg-sky-500 text-white p-5 py-3 rounded-lg font-semibold shadow-lg">
+              Criar <FontAwesomeIcon icon={faPlusCircle} />
               {/* adicionar icone de plus(+) */}
             </button>
           </form>
@@ -62,25 +68,43 @@ function App() {
           <div key={todo.id} className="w-1/2 mb-3">
             <div
               className="bg-zinc-800 border-none outline-none rounded-lg p-4 flex
-              items-center"
+              items-center shadow-sm shadow-zinc-950 justify-between relative"
             >
-              {/* display flex, align item center, justify-content:space between */}
               <input
-                className="cursor-pointer"
+                className="cursor-pointer appearance-none border-2 w-4 h-4 rounded-xl border-sky-400 hover:border-sky-400/60 hover:bg-sky-500/15 checked:bg-violet-400/85 checked:border-0 checked:hover:bg-violet-400 peer"
                 type="checkbox"
                 // ref={completeRef}
                 checked={todo.completed}
                 onChange={() => toggleComplete(todo.id)}
               />
+              <svg
+                className="
+      absolute 
+      w-2 h-3 ml-1
+      text-white  hidden peer-checked:block pointer-events-none"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="4"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <polyline points="20 6 9 17 4 12"></polyline>
+              </svg>
+
               <h2
-                className={`text-white ${
-                  todo.completed ? "line-through text-gray-400" : ""
+                className={`text-white font-medium ${
+                  todo.completed ? "line-through text-zinc-500" : ""
                 }`}
               >
                 {todo.task}
               </h2>
               <button onClick={() => removeTodo(todo.id)}>
-                <span>ICONE</span>
+                <FontAwesomeIcon
+                  icon={faTrashCan}
+                  className="text-neutral-500 hover:text-red-500 hover:bg-neutral-700 p-1 rounded-sm w-4 h-4"
+                />
                 {/* Adicionar icone lixeira */}
               </button>
             </div>
