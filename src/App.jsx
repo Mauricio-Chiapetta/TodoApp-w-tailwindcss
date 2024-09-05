f82f50f509dc66bb0e16baf2c392a671f19369e3
Mauricio Chiapetta <m.faisca1234@gmail.com>
2024-09-05 01:20:15 -0300

    some comments and css fix hovers,transition,etc

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,12 @@ import { useRef } from "react";
 function App() {
   const [todos, setTodos] = useState([]);
   const [task, setTask] = useState("");
-  const completeRef = useRef(null);
+  // const completeRef = useRef(null);
 
   const addTodos = (task) => {
     const id = Math.floor(Math.random() * 1000000);
     const newTodo = { id, task, completed: false };
-    setTodos((state) => [newTodo, ...state]);
+    setTodos((state) => [...state, newTodo]);
   };
 
   const removeTodo = (id) => {
@@ -36,8 +36,8 @@ function App() {
   return (
     <>
       <Navbar />
-      <div className="relative"> {/* Container com posição relativa */}
-        <div className="absolute bottom-10 left-0 right-0 flex items-center justify-center p-4 ">
+      <div className="relative">
+        <div className="absolute -bottom-8 left-0 right-0 flex items-center justify-center p-4 ">
           <form
             className="flex items-center justify-center w-full max-w-full "
             onSubmit={handleSubmit}
@@ -48,34 +48,44 @@ function App() {
               value={task}
               onChange={(e) => setTask(e.target.value)}
               placeholder="Adicione uma nova tarefa"
-              className="bg-zinc-800 border-none text-white placeholder-zinc-700 outline-none focus:border-purple-800 focus:ring-1 focus:ring-violet-400 rounded-lg px-2 py-3 mr-2 w-5/12"
+              className="bg-zinc-800 border-none text-white placeholder-zinc-500 font-medium outline-none focus:border-purple-800 focus:ring-1 focus:ring-violet-400 rounded-lg px-2 py-3 mr-2 w-5/12"
             />
-            <button className="bg-sky-600 hover:bg-sky-500 text-white p-5 py-3 rounded-lg font-semibold">Criar<span>+</span></button>
+            <button className="transition duration-300 bg-sky-600 hover:bg-sky-500 text-white p-5 py-3 rounded-lg font-semibold">
+              Criar<span>+</span>
+              {/* adicionar icone de plus(+) */}
+            </button>
           </form>
-        </div> 
-
-        <div className="pt-20"> {/* Espaço adicional para evitar sobreposição do conteúdo */}
-          {todos.map((todo) => (
-            <div key={todo.id}>
-              <div>
-                <input
-                  type="checkbox" 
-                  ref={completeRef}
-                  checked={todo.completed}
-                  onChange={() => toggleComplete(todo.id)}
-                />
-                <h2
-                  className={`text-white ${
-                    todo.completed ? "line-through text-gray-400" : ""
-                  }`}
-                >
-                  {todo.task}
-                </h2>
-                <button onClick={() => removeTodo(todo.id)}>Remover</button>
-              </div>
-            </div>
-          ))}
         </div>
+      </div>
+      <div className="flex items-center justify-center w-full max-w-full flex-col mt-8">
+        {todos.map((todo) => (
+          <div key={todo.id} className="w-1/2 mb-3">
+            <div
+              className="bg-zinc-800 border-none outline-none rounded-lg p-4 flex
+              items-center"
+            >
+              {/* display flex, align item center, justify-content:space between */}
+              <input
+                className="cursor-pointer"
+                type="checkbox"
+                // ref={completeRef}
+                checked={todo.completed}
+                onChange={() => toggleComplete(todo.id)}
+              />
+              <h2
+                className={`text-white ${
+                  todo.completed ? "line-through text-gray-400" : ""
+                }`}
+              >
+                {todo.task}
+              </h2>
+              <button onClick={() => removeTodo(todo.id)}>
+                <span>ICONE</span>
+                {/* Adicionar icone lixeira */}
+              </button>
+            </div>
+          </div>
+        ))}
       </div>
     </>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ export default function Navbar(){
 
     return (
 <div className="h-48 bg-neutral-950 w-full flex items-center justify-center px-4">
-      <h1 className="text-sky-400 text-6xl font-black">to<span className="text-indigo-600">do</span></h1>
+      <h1 className="text-sky-400 text-6xl font-black">to<span className="text-violet-500">do</span></h1>
     </div>
     )
    
