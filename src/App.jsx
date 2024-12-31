709aec0f85f916a3b971e130a504f97a14055ded
Mauricio Chiapetta <m.faisca1234@gmail.com>
2024-12-31 19:06:05 -0300

    feat: add feature to display completed tasks in 'X of Y' format
    - Show number of completed tasks (X) out of total tasks (Y)
    - Updated UI to reflect the new format in the "Completed Tasks" section

--- src/App.jsx
+++ src/App.jsx
@@ -26,13 +26,34 @@ function App() {
     );
   };
 
+  const completedTodos = todos.length;
+  const checkedTodos = todos.filter((todo) => todo.completed).length;
+
   return (
     <>
       <Navbar />
       <div className="relative">
         <Form addTodos={addTodos} />
       </div>
-      <div className="flex items-center justify-center w-full max-w-full flex-col mt-8">
+
+      <div className="flex  justify-between items-center mt-9 sm:px-96 px-4 mb-5">
+        <h1 className="text-sky-400 font-bold">
+          Tarefas Criadas{" "}
+          <span className="text-white bg-zinc-700 px-3 py-1 rounded-full font-medium">
+            {completedTodos}
+          </span>
+        </h1>
+
+        <h1 className="text-violet-500 font-bold">
+          Tarefas Concluídas{" "}
+          <span className="text-white bg-zinc-700 px-3 py-1 rounded-full font-medium">
+            {checkedTodos} de {completedTodos}
+          </span>
+        </h1>
+      </div>
+
+      {/* Lista de tarefas */}
+      <div className="flex items-center justify-center w-full max-w-full flex-col">
         {todos.length > 0 ? (
           todos.map((todo) => (
             <Todos
@@ -45,16 +66,16 @@ function App() {
             />
           ))
         ) : (
-          <div className="flex flex-col items-center mt-20 justify-center text-center">
+          <div className="flex flex-col items-center justify-center text-center w-full max-w-md mx-auto border-t-2 border-zinc-700 rounded-lg mt-5 px-4 py-6">
             <FontAwesomeIcon
               icon={faClipboard}
-              className="text-zinc-500  h-16"
+              className="text-zinc-500 h-16"
             />
-            <h3 className="text-zinc-500 font-extrabold text-lg">
+            <h3 className="text-zinc-500 font-extrabold text-lg mt-4">
               Você ainda não tem tarefas cadastradas
             </h3>
             <p className="text-zinc-500 font-medium">
-              Crie tarefas e organize seus items a fazer
+              Crie tarefas e organize seus itens a fazer
             </p>
           </div>
         )}
